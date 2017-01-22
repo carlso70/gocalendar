0cddd2f86f15755a919cf2790adb069b3b0817f5
Jimmy Carlson <carlso70@purdue.edu>
2017-01-21 23:12:36 -0500

    Basic calendar setup

--- gocal.go
+++ gocal.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	//"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/calendar/v3"
 	"gopkg.in/urfave/cli.v1"
 	"os"
 )
@@ -18,8 +18,40 @@ func main() {
 
 	app.Action = func(c *cli.Context) error {
 		fmt.Printf("Hello %q", c.Args().Get(0))
+		addCalendar(c.Args().Get(0), c.Args().Get(1), c.Args().Get(2), c.Args().Get(3))
 		return nil
 	}
 
 	app.Run(os.Args)
+}
+
+func addCalendar(summary string, location string, description string, calId string) {
+	// Refer to the Go quickstart on how to setup the environment:
+	// https://developers.google.com/google-apps/calendar/quickstart/go
+	// Change the scope to calendar.CalendarScope and delete any stored credentials.
+
+	event := &calendar.Event{
+		Summary:     summary,
+		Location:    location,
+		Description: description,
+		Start: &calendar.EventDateTime{
+			DateTime: "2015-05-28T09:00:00-07:00",
+			TimeZone: "America/Los_Angeles",
+		},
+		End: &calendar.EventDateTime{
+			DateTime: "2015-05-28T17:00:00-07:00",
+			TimeZone: "America/Los_Angeles",
+		},
+		Attendees: []*calendar.EventAttendee{
+			&calendar.EventAttendee{Email: "lpage@example.com"},
+			&calendar.EventAttendee{Email: "sbrin@example.com"},
+		},
+	}
+
+	calendarId := calId
+	event, err = srv.Events.Insert(calendarId, event).Do()
+	if err != nil {
+		log.Fatalf("Unable to create event. %v\n", err)
+	}
+	fmt.Printf("Event created: %s\n", event.HtmlLink)
 }
