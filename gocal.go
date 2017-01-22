355358e0a581196e8d26c1413bb1202086aa456e
Jimmy Carlson <carlso70@purdue.edu>
2017-01-22 02:46:03 -0500

    Basic add functionality

--- gocal.go
+++ gocal.go
@@ -181,6 +181,32 @@ func main() {
 			Aliases: []string{"a"},
 			Usage:   "add an appointment on your calendar",
 			Action: func(c *cli.Context) error {
+
+				event := &calendar.Event{
+					Summary:     "Testing",
+					Location:    "800 Howard St., San Francisco, CA 94103",
+					Description: "A chance to hear more about Google's developer products.",
+					Start: &calendar.EventDateTime{
+						DateTime: "2015-05-28T09:00:00-07:00",
+						TimeZone: "America/Los_Angeles",
+					},
+					End: &calendar.EventDateTime{
+						DateTime: "2015-05-28T17:00:00-07:00",
+						TimeZone: "America/Los_Angeles",
+					},
+					Recurrence: []string{"RRULE:FREQ=DAILY;COUNT=2"},
+					Attendees: []*calendar.EventAttendee{
+						&calendar.EventAttendee{Email: "lpage@example.com"},
+						&calendar.EventAttendee{Email: "sbrin@example.com"},
+					},
+				}
+
+				calendarId := "primary"
+				event, err = srv.Events.Insert(calendarId, event).Do()
+				if err != nil {
+					log.Fatalf("Unable to create event. %v\n", err)
+				}
+				fmt.Printf("Event created: %s\n", event.HtmlLink)
 				return nil
 			},
 		},
