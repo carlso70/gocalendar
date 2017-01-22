6e13200038a4e559e90903a25c364c7f36a85348
Jimmy Carlson <carlso70@purdue.edu>
2017-01-21 22:54:41 -0500

    Basic CLI

--- gocal.go
+++ gocal.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
-	"google.golang.org/api/calendar/v3"
-	"gopkg.in/urfave/cli.v2"
+	//"google.golang.org/api/calendar/v3"
+	"gopkg.in/urfave/cli.v1"
+	"os"
 )
 
 func main() {
-	fmt.Println("vim-go")
+	app := cli.NewApp()
+	app.Name = "Calendar"
+	app.Usage = "Add calendar events from the command line"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("boom! I say!")
+		return nil
+	}
 
+	app.Action = func(c *cli.Context) error {
+		fmt.Printf("Hello %q", c.Args().Get(0))
+		return nil
+	}
+
+	app.Run(os.Args)
 }
